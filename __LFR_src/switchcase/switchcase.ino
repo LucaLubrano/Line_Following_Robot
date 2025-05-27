3f9048976004d3cdf01d2543529b5cbf500088a1
Luca Lubrano Lavadera <lucalub123@gmail.com>
2025-05-27 14:38:00 +1000

    switchcase

--- __LFR_src/switchcase/switchcase.ino
+++ __LFR_src/switchcase/switchcase.ino
@@ -35,7 +35,8 @@
 #define DETECT_BLACK(sensor) (sensor < BLACK_LINE_THRESHOLD)
 #define DETECT_RED(sensor) (sensor < RED_LIGHT_LEVEL)
 #define DETECT_GREEN(sensor) (sensor < GREEN_LIGHT_LEVEL)
-#define DETECT_TURNING_MARKER detect_rising_edge(line_sensor_array[6], line_sensor_array[7])
+#define DETECT_TURNING_MARKER !detect_rising_edge(side_sensor_array[0], side_sensor_array[1])
+// #define DETECT_TURNING_MARKER side_sensor_array[0]
 #define DETECT_OBSTACLE 0 
 #define PB_PRESSED 0 
 // #define STRAIGHT_IR_CONFIG (DETECT_BLACK(sensor_array.CNTR_IR1) && DETECT_WHITE(sensor_array.CNTR_IR2) && DETECT_WHITE(sensor_array.CNTR_IR3) && DETECT_BLACK(sensor_array.CNTR_IR4))
@@ -65,8 +66,8 @@
 /* Motor pins */ 
 
 // check these values through testing
-#define STRAIGHT_SPEED 128
-#define SLOW_ZONE_SPEED 64
+#define STRAIGHT_SPEED 64
+#define SLOW_ZONE_SPEED 32
 #define TURN_SPEED 96
 #define TURN_SPEED_INCREMENT 2
 #define TRACK_ADJUSTMENT_SPEED 1
@@ -82,9 +83,9 @@
 // #define PROPORTIONAL_COEFFICIENT 22
 // #define INTEGRAL_COEFFICIENT 0.0001
 // #define DERIVATIVE_COEFFICIENT 6
-#define PROPORTIONAL_COEFFICIENT 1
+#define PROPORTIONAL_COEFFICIENT 20
 #define INTEGRAL_COEFFICIENT 0
-#define DERIVATIVE_COEFFICIENT 0
+#define DERIVATIVE_COEFFICIENT 4
 
 // P > D
 // D about 1/4 
@@ -93,23 +94,23 @@
 // use I 
 
 /* Light Setup */
-#define STOP_LED_DDR DDRE
+#define STOP_LED_DDR DDRB
 #define STRAIGHT_LED_DDR DDRB
 #define OBSTACLE_LED_DDR DDRB
-#define TURRET_ON_DDR DDRB
-#define TURRET_TRACKING_DDR DDRD
+#define TURRET_ON_DDR DDRC
+#define TURRET_TRACKING_DDR DDRC
 
-#define STOP_LED_PORT PORTE
+#define STOP_LED_PORT PORTB
 #define STRAIGHT_LED_PORT PORTB
 #define OBSTACLE_LED_PORT PORTB
-#define TURRET_ON_PORT PORTB
-#define TURRET_TRACKING_PORT PORTD
+#define TURRET_ON_PORT PORTC
+#define TURRET_TRACKING_PORT PORTC
 
-#define STOP_LED_PIN PIN6
-#define STRAIGHT_LED_PIN PIN0
+#define STOP_LED_PIN PIN3
+#define STRAIGHT_LED_PIN PIN2
 #define OBSTACLE_LED_PIN PIN1
-#define TURRET_ON_PIN PIN7
-#define TURRET_TRACKING_PIN PIN0
+#define TURRET_ON_PIN PIN6
+#define TURRET_TRACKING_PIN PIN7
 
 ////////////////////////////////////////////////
 // Type Definitions
@@ -118,9 +119,16 @@
 typedef enum {
   IDLE,
   STRAIGHT,
+  SRT_TO_TRN_STATE,
+  TRN_TO_SRT_STATE,
   TURNING,
   SLOW_ZONE_STRAIGHT,
   SLOW_ZONE_TURNING,
+  ENTER_INIT_SLOW_ZONE,
+  EXIT_INIT_SLOW_ZONE,
+  ENTER_SLOW_ZONE,
+  EXIT_SLOW_ZONE,
+  INITIAL_SLOW_ZONE,
 } BOT_STATE_TYPE;
 
 typedef enum {
@@ -171,7 +179,7 @@ typedef enum{
 void timer_init(void);
 void ADC_init(void);
 void MOTOR_PWM_init(void);
-void bot_state_machine(void);
+void robot_state_machine(void);
 void motor_state_machine(void);
 void line_ir_state_machine(void);
 void PID_calc(void);
@@ -193,7 +201,7 @@ int detect_rising_edge(int current_sensor1, int current_sensor2);
 
 volatile BOT_STATE_TYPE bot_state = STRAIGHT; 
 volatile int side_sensor_array[2] = {0,0};
-volatile int line_sensor_array[6] = {0,0,0,0,0,0};
+volatile int line_sensor_array[4] = {0,0,0,0};
 volatile int colour_sensor_array[3] = {0,0,0};
 volatile MOTOR_STATE_TYPE motor_state = MTR_OFF;
 volatile LINE_IR_TYPE line_ir_state = ADC4;
@@ -207,8 +215,8 @@ volatile float P, I, D, LP;
 const float Kp = PROPORTIONAL_COEFFICIENT;
 const float Ki = INTEGRAL_COEFFICIENT;
 const float Kd = DERIVATIVE_COEFFICIENT;
-const int maxspeed = 128;
-const int basespeed = 128;
+volatile int maxspeed = 128;
+volatile int basespeed = 128;
 volatile int r_mtr_speed, l_mtr_speed;
 volatile float error, correction, sp;
 volatile int position, sensor_average, sensor_sum;
@@ -219,6 +227,8 @@ volatile int prev_sensor1 = 0;
 volatile int prev_sensor2 = 0;
 // volatile int current_corner_sensor = 0;
 volatile bool signal_from_turret = false;
+volatile int slow_zone_count = 0;
+volatile bool slow_zone_check = false;
 
 ////////////////////////////////////////////////
 // Main
@@ -228,19 +238,22 @@ void setup() {
   // Pause interupts before initialisation
   cli();
   // Initialisation
-  ADC_init();
+  ADC_init(); 
   MOTOR_PWM_init();
   // COMMS_init();
   LED_init();
   // Reset interupts after initialisation
   sei();
   Serial1.begin(9600);
-  // Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop(){
   PID_calc();
   turn_calc();
+
+  // robot_state_machine();
+
   // Serial.println("--------------------");
   // Serial.println(line_sensor_array[0]);
   // Serial.println(line_sensor_array[1]);
@@ -278,36 +291,75 @@ void robot_state_machine(void){
   switch(bot_state){
     case IDLE:
       SET1_LED(STRAIGHT_LED_PORT, STRAIGHT_LED_PIN);
-      bot_state = STRAIGHT;
+      // max_speed = SLOW_ZONE_SPEED;
+      SETBIT(PORTB, PIN1);
+      maxspeed = SLOW_ZONE_SPEED;
+      bot_state = INITIAL_SLOW_ZONE;
+    break;
+
+    case INITIAL_SLOW_ZONE:
+      PID_calc();
+      turn_calc();
+      if(colour_sensor_array[0]){
+        bot_state = EXIT_INIT_SLOW_ZONE;
+        maxspeed = STRAIGHT_SPEED;
+      }
     break;
     
     case STRAIGHT:
       PID_calc();
       turn_calc();
-      if(DETECT_TURNING_MARKER){
+      // if(DETECT_TURNING_MARKER){
+      if(side_sensor_array[0]){  
         CLEAR_LEDS();
-        bot_state = TURNING;
+        bot_state = SRT_TO_TRN_STATE;
       }
-      if(DETECT_RED(0)){
+      if(colour_sensor_array[0]){
         Serial1.println('1');
         SET2_LED(STRAIGHT_LED_PORT, STRAIGHT_LED_PIN, TURRET_ON_PORT, TURRET_ON_PIN);
-        bot_state = SLOW_ZONE_STRAIGHT;
-        set_motors(SLOW_ZONE_MTR_SPEED, SLOW_ZONE_MTR_SPEED);
+        bot_state = ENTER_SLOW_ZONE;
+        maxspeed = SLOW_ZONE_SPEED;
+      } 
+    break;
+
+    case SRT_TO_TRN_STATE:
+      PID_calc();
+      turn_calc();
+      if (!side_sensor_array[0]){
+        if (!slow_zone_check){
+          bot_state = TURNING;  
+        } else {
+          bot_state = SLOW_ZONE_TURNING;
+        }
+      } 
+    break;
+
+    case TRN_TO_SRT_STATE:
+      PID_calc();
+      turn_calc();
+      if (!side_sensor_array[0]){
+        if (!slow_zone_check){
+          bot_state = STRAIGHT;  
+        } else {
+          bot_state = SLOW_ZONE_STRAIGHT;
+        }
       } 
     break;
 
     case TURNING:
       PID_calc();
       turn_calc();
-      if(DETECT_TURNING_MARKER){
-        SET1_LED(STRAIGHT_LED_PORT, STRAIGHT_LED_PIN);
-        bot_state = STRAIGHT;
+      // if(DETECT_TURNING_MARKER){
+      if(side_sensor_array[0]){
+        // SET1_LED(STRAIGHT_LED_PORT, STRAIGHT_LED_PIN);
+        SETBIT(PORTB, PIN1);
+        bot_state = TRN_TO_SRT_STATE;
       }
-      if(DETECT_RED(0)){
+      if(ENTER_SLOW_ZONE){
         Serial1.println('1');
         SET1_LED(TURRET_ON_PORT, TURRET_ON_PIN);
         bot_state = SLOW_ZONE_TURNING;
-        set_motors(SLOW_ZONE_MTR_SPEED, SLOW_ZONE_MTR_SPEED);
+        maxspeed = SLOW_ZONE_SPEED;
       } 
     break;
 
@@ -317,7 +369,7 @@ void robot_state_machine(void){
 
       signal_from_turret = check_input_signal();
 
-      if(DETECT_TURNING_MARKER){
+      if(side_sensor_array[0]){
         if(signal_from_turret){
           SET2_LED(TURRET_ON_PORT, TURRET_ON_PIN, TURRET_TRACKING_PORT, TURRET_TRACKING_PIN);
         } else{
@@ -363,20 +415,56 @@ void robot_state_machine(void){
         set_motors(SPEED_LIMIT, SPEED_LIMIT);
       } 
     
+    break;  
+    
+    case EXIT_INIT_SLOW_ZONE:
+      PID_calc();
+      turn_calc();
+      if (!colour_sensor_array[0]){
+        slow_zone_count = 1;
+        bot_state = STRAIGHT;
+      } 
+    break;
+    
+    case ENTER_INIT_SLOW_ZONE:
+      PID_calc();
+      turn_calc();
+      if (!colour_sensor_array[0]){
+        slow_zone_count = 4;
+        bot_state = INITIAL_SLOW_ZONE;
+      }
+    break;
+
+    case ENTER_SLOW_ZONE:
+      PID_calc();
+      turn_calc();
+      if (!colour_sensor_array[0]){
+        slow_zone_count = 2;
+        bot_state = SLOW_ZONE_STRAIGHT;
+      }
+    break;
+    
+    case EXIT_SLOW_ZONE:
+      PID_calc();
+      turn_calc();
+      if (!colour_sensor_array[0]){
+        slow_zone_count = 3;
+        bot_state = STRAIGHT;
+      }
     break;
   }
 }
-
-
 // todo: make the pwm init run when a button is pressed
 void sensor_value_average(void){
   sensor_average = 0;
   sensor_sum = 0;
   // todo: (?) make a for loop?
-  sensor_average = (line_sensor_array[0] * -3 ) + (line_sensor_array[1] * -2 ) + (line_sensor_array[2] * -1 ) + (line_sensor_array[3] * 1 ) + (line_sensor_array[4] * 2 ) + (line_sensor_array[5] * 3 );
+  sensor_average = (line_sensor_array[0] * -2 * 1000) + (line_sensor_array[1] * -1 * 1000) + (line_sensor_array[2] * 1 * 1000) + (line_sensor_array[3] * 2 * 1000);
   // sensor_average = (line_sensor_array[0] * -2) + (line_sensor_array[1] * -1) + (line_sensor_array[2] * 1) + (line_sensor_array[3] * 2);
-  sensor_sum = line_sensor_array[0] + line_sensor_array[1] + line_sensor_array[2] + line_sensor_array[3] + line_sensor_array[4] + line_sensor_array[5];
+  sensor_sum = line_sensor_array[0] + line_sensor_array[1] + line_sensor_array[2] + line_sensor_array[3];
   if (sensor_average == 0){
+    position = 0;
+  } else if ((sensor_average == 0) & (side_sensor_array[0] == 1) ){
     position = 0;
   } else {
     position = sensor_average / sensor_sum;
@@ -473,12 +561,6 @@ void ADC_init(void){
   // Start ADC
   ADCSRA |= (1<<ADSC);  
 }
-// void COMMS_init(void){
-//   SETBIT(TURRET_INPUT_DDR, TURRET_INPUT_PIN); // Set SIG TRK to input
-//   SETBIT(TURRET_OUTPUT_DDR, TURRET_OUTPUT_PIN); // Set SIG SZ to input
-//   CLRBIT(TURRET_INPUT_PORT, TURRET_INPUT_PIN); // Set to low
-//   CLRBIT(TURRET_OUTPUT_PORT, TURRET_OUTPUT_PIN); // Set to low
-// }
 void MOTOR_PWM_init(void){
   // Set pins b7 and d0 to output to expose pwm signal to pin
   SETBIT(DDRB, PB7);
@@ -507,6 +589,7 @@ void LED_init(void){
 }
 void CLEAR_LEDS(void){
   // Set to off
+  // SETBIT(DDRB, PIN1);
   CLRBIT(STOP_LED_PORT, STOP_LED_PIN);
   CLRBIT(STRAIGHT_LED_PORT, STRAIGHT_LED_PIN);
   CLRBIT(OBSTACLE_LED_PORT, OBSTACLE_LED_PIN);
@@ -537,8 +620,8 @@ void SET4_LED(int PORT_1, int PIN_1, int PORT_2, int PIN_2, int PORT_3, int PIN_
 }
 int detect_rising_edge(int current_sensor1, int current_sensor2){
   int val_changed = 0; int rising_edge = 0;
-  val_changed = (prev_sensor1 ^ current_sensor1) | (prev_sensor2 ^ current_sensor2);
-  rising_edge = (val_changed & current_sensor1) | (val_changed & current_sensor2);
+  val_changed = (!prev_sensor1 ^ !current_sensor1);
+  rising_edge = (val_changed & !current_sensor1);
   return rising_edge;
 } 
 bool check_input_signal(void){
@@ -550,6 +633,13 @@ bool check_input_signal(void){
       else if (receivedData == '0') {
         return false;
       }
+  }
+}
+int check_slow_zone_state(void){
+  if (slow_zone_count == 1){
+    maxspeed = STRAIGHT_SPEED;
+  } else if (slow_zone_count == 2){
+    
   }
 }
 ////////////////////////////////////////////////
@@ -570,47 +660,50 @@ ISR(ADC_vect){
 
       switch (line_ir_state){ 
         case ADC4: // Rightmost sensor
-        line_sensor_array[2] = temp_adc;  
+        line_sensor_array[0] = temp_adc;
         SET_MUX(IR2_MUX);
         ADCSRB &= ~(1<<MUX5);
         line_ir_state = ADC5;
         break;
         
         case ADC5: // Second Rightmost sensor
-        line_sensor_array[5] = temp_adc;
+        line_sensor_array[1] = temp_adc;
         SET_MUX(IR3_MUX);
         line_ir_state = ADC6;
         break;
         
         case ADC6: // Rightmost center sensor
-        line_sensor_array[4] = temp_adc;
+        prev_sensor1 = side_sensor_array[0];
+        side_sensor_array[0] = temp_adc;
+        // line_sensor_array[4] = temp_adc;
         SET_MUX(IR4_MUX);
         line_ir_state = ADC7;
         break;
         
         case ADC7: // Second Rightmost center sensor
-        line_sensor_array[0] = temp_adc;
+        side_sensor_array[1] = temp_adc;
         SET_MUX(IR5_MUX);
+        // line_sensor_array[3] = temp_adc;
         ADCSRB |= (1<<MUX5);
         line_ir_state = ADC11;
         break;
         
         case ADC11: // Second Leftmost center sensor
-        line_sensor_array[1] = temp_adc;
+        line_sensor_array[2] = temp_adc;
         SET_MUX(IR6_MUX);
         ADCSRB |= (1<<MUX5);
         line_ir_state = ADC10;
         break;
         
         case ADC10: // Leftmost center sensor
-        line_sensor_array[3] = temp_adc;
+        // line_sensor_array[3] = temp_adc;
         SET_MUX(IR7_MUX);
         ADCSRB |= (1<<MUX5);
         line_ir_state = ADC9;
         break;
         
         case ADC9: // second Leftmost sensor
-        // line_sensor_array[2] = temp_adc;
+        line_sensor_array[1] = temp_adc;
         CLEAR_MUX;
         ADCSRB |= (1<<MUX5);
         line_ir_state = ADC8;
